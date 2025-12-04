2e710546d2c9b0ebda456b0b860c7d5fee64eca5
Oscar Barrios <obarrios@suse.com>
2025-12-04 15:57:09 +0100

    Add security steps

--- testsuite/step_definitions/security_steps.ts
+++ testsuite/step_definitions/security_steps.ts
@@ -1,53 +1,34 @@
 import {Then, When} from '@cucumber/cucumber';
+import {expect} from '@playwright/test';
+import {globalVars, getContext, addContext, getBrowserInstances} from '../helpers/index.js';
 
+const static_resources: { [key: string]: string } = {
+  'img': 'action-add.gif',
+  'css': 'susemanager-sp-migration.css',
+  'fonts': 'DroidSans.ttf',
+  'javascript': 'actionchain.js'
+};
 
-When(/^I retrieve a "(.*)" static resource$/, async function (resource_type) {
-    // TODO: Port this step body to Playwright/Helpers
-    // Original Ruby implementation (commented line-by-line to avoid '*/' sequences in paths):
-    //   static_resources = {
-    //     'img' => 'action-add.gif',
-    //     'css' => 'susemanager-sp-migration.css',
-    //     'fonts' => 'DroidSans.ttf',
-    //     'javascript' => 'actionchain.js'
-    //   }
-    //   @url = "#{Capybara.app_host}/#{resource_type}/#{static_resources[resource_type]}"
-    //   URI.open(@url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE) do |file|
-    //     @headers = file.meta
-    //   end
-    //
-    throw new Error('Step not yet implemented (auto-generated)')
+When(/^I retrieve a "(.*)" static resource$/, async function (resource_type: string) {
+  addContext('url', `${process.env.BASE_URL}/${resource_type}/${static_resources[resource_type]}`);
+  addContext('response', await getBrowserInstances().context.request.get(<string>getContext('url'), { ignoreHTTPSErrors: true }));
+  addContext('headers', await getContext('response').headers());
 });
 
-Then(/^the response header "(.*?)" should be "(.*?)"$/, async function (name, value) {
-    // TODO: Port this step body to Playwright/Helpers
-    // Original Ruby implementation (commented line-by-line to avoid '*/' sequences in paths):
-    //   assert_includes(@headers.keys, name.downcase, "Header '#{name}' not present in '#{@url}'")
-    //   assert_equal(value, @headers[name.downcase], "Header '#{name}' in '#{@url}' is not '#{value}'")
-    //
-    throw new Error('Step not yet implemented (auto-generated)')
+Then(/^the response header "(.*?)" should be "(.*?)"$/, async function (name: string, value: string) {
+  expect(getContext('headers')).toHaveProperty(name.toLowerCase(), `Header '${name}' not present in '${getContext('url')}'`);
+  expect(getContext('headers')[name.toLowerCase()]).toEqual(value);
 });
 
-Then(/^the response header "(.*?)" should not be "(.*?)"$/, async function (name, value) {
-    // TODO: Port this step body to Playwright/Helpers
-    // Original Ruby implementation (commented line-by-line to avoid '*/' sequences in paths):
-    //   refute_equal(value, @headers[name.downcase], "Header '#{name}' in '#{@url}' is '#{value}'")
-    //
-    throw new Error('Step not yet implemented (auto-generated)')
+Then(/^the response header "(.*?)" should not be "(.*?)"$/, async function (name: string, value: string) {
+  expect(getContext('headers')[name.toLowerCase()]).not.toEqual(value);
 });
 
-Then(/^the response header "(.*?)" should contain "(.*?)"$/, async function (name, value) {
-    // TODO: Port this step body to Playwright/Helpers
-    // Original Ruby implementation (commented line-by-line to avoid '*/' sequences in paths):
-    //   assert_includes(@headers.keys, name.downcase, "Header '#{name}' not present in '#{@url}'")
-    //   assert_includes(@headers[name.downcase], value, "Header '#{name}' in '#{@url}' does not contain '#{value}'")
-    //
-    throw new Error('Step not yet implemented (auto-generated)')
+Then(/^the response header "(.*?)" should contain "(.*?)"$/, async function (name: string, value: string) {
+  expect(getContext('headers')).toHaveProperty(name.toLowerCase(), `Header '${name}' not present in '${getContext('url')}'`);
+  expect(getContext('headers')[name.toLowerCase()]).toContain(value);
 });
 
-Then(/^the response header "(.*?)" should not be present$/, async function (name) {
-    // TODO: Port this step body to Playwright/Helpers
-    // Original Ruby implementation (commented line-by-line to avoid '*/' sequences in paths):
-    //   refute_includes(@headers.keys, name.downcase, "Header '#{name}' present in '#{@url}'")
-    //
-    throw new Error('Step not yet implemented (auto-generated)')
+Then(/^the response header "(.*?)" should not be present$/, async function (name: string) {
+  expect(getContext('headers')).not.toHaveProperty(name.toLowerCase(), `Header '${name}' present in '${getContext('url')}'`);
 });
